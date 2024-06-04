ac755873e73a06fe1bb35e5647e032e7987fa6d6
NikitaTaverz <nikita3kovalev3@gmail.com>
2024-06-04 17:49:30 +0300

    update / delete https -> dio / check network update

--- atol_online_dart/lib/atol_online_v1_4/api/api_check_request_impl.dart
+++ atol_online_dart/lib/atol_online_v1_4/api/api_check_request_impl.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:atol_online_dart/atol_online_v1_4/api/api_check_request.dart';
 import 'package:atol_online_dart/atol_online_v1_4/model/request_model_exchange_info.dart';
 import 'package:atol_online_dart/atol_online_v1_4/utils/const_app.dart';
-import 'package:http/http.dart' as httpImport;
+// import 'package:http/http.dart' as httpImport;
+import 'package:dio/dio.dart' as httpDio;
 
 class ApiRequestAtolCheckImpl implements ApiRequestAtolCheck {
   late String? tokenCurrent;
@@ -29,28 +30,66 @@ class ApiRequestAtolCheckImpl implements ApiRequestAtolCheck {
     }
     final Map<String, dynamic> bodyParams = model.toMap();
 
-    final url = Uri.https(
-      ConstantApp.mainUrl,
-      '/${ConstantApp.postMainUrl}/${ConstantApp.version}/$codeGroup/sell',
-    );
+    // final url = Uri.https(
+    //   ConstantApp.mainUrl,
+    //   '/${ConstantApp.postMainUrl}/${ConstantApp.version}/$codeGroup/sell',
+    // );
     final headerParam = {
       'Content-Type':
           'application/json; charset=utf-8', //'application/json; charset=utf-8'
       'Token': tokenCurrent!
     };
 
-    final response = await httpImport.post(
-      url,
-      headers: headerParam,
-      body: jsonEncode(bodyParams),
+    final response = await httpDio.Dio().post(
+      '/${ConstantApp.postMainUrl}/${ConstantApp.version}/$codeGroup/sell',
+      queryParameters: headerParam,
+      data: jsonEncode(bodyParams),
     );
-    final decode = jsonDecode(response.body);
+    final decode = jsonDecode(response.data);
     if (response.statusCode == 200) {
       return decode;
     } else {
       throw Exception('Failed to createCheck');
     }
   }
+
+  //   /// ### Пример регистрации чека с операцией «Приход»:
+  // /// ```
+  // ///  https://online.atol.ru/possystem/v5/group1/sell?token=<token>
+  // /// ````
+  // ///
+  // /// Заголовок запроса должен содержать параметры:
+  // /// * Content-type: application/json; charset=utf-8
+  // /// * Token: <token>
+  // @override
+  // Future<Map> createCheck(ExchangeInfo model, String codeGroup) async {
+  //   if(tokenCurrent != null){
+  //     throw Exception('Emtpy token');
+  //   }
+  //   final Map<String, dynamic> bodyParams = model.toMap();
+
+  //   final url = Uri.https(
+  //     ConstantApp.mainUrl,
+  //     '/${ConstantApp.postMainUrl}/${ConstantApp.version}/$codeGroup/sell',
+  //   );
+  //   final headerParam = {
+  //     'Content-Type':
+  //         'application/json; charset=utf-8', //'application/json; charset=utf-8'
+  //     'Token': tokenCurrent!
+  //   };
+
+  //   final response = await httpImport.post(
+  //     url,
+  //     headers: headerParam,
+  //     body: jsonEncode(bodyParams),
+  //   );
+  //   final decode = jsonDecode(response.body);
+  //   if (response.statusCode == 200) {
+  //     return decode;
+  //   } else {
+  //     throw Exception('Failed to createCheck');
+  //   }
+  // }
 
   @override
   Future<dynamic> returnCheck() {
--- atol_online_dart/lib/check_network.dart
+++ atol_online_dart/lib/check_network.dart
@@ -1,20 +1,50 @@
+// import 'package:http/http.dart' as httpImport;
 
-import 'package:http/http.dart' as httpImport;
+import 'dart:io';
 
 class CheckNetworkCustom {
   static Future<bool> checkNetwork() async {
+    // try {
+    //   /// Ping Yandex from check network )))
+    //   final response = await httpImport.post(
+    //     Uri.https('dzen.ru'),
+    //   );
+    //   if (response.statusCode == 200 || response.statusCode == 302) {
+    //     return true;
+    //   }
+    // } catch (e) {
+    //   throw Exception('Network connect error');
+    //   //  throw Exception('No connect network');
+    // }
+    return _checkConnection();
+  }
+
+  static Future<bool> _checkConnection() async {
+    // ignore: inference_failure_on_instance_creation
+    await Future.delayed(const Duration(seconds: 1));
+    bool connect = true;
     try {
-      /// Ping Yandex from check network )))
-      final response = await httpImport.post(
-        Uri.https('dzen.ru'),
-      );
-      if (response.statusCode == 200 || response.statusCode == 302) {
-        return true;
+      // final check1 = await updateConnectionStatus(await connectivity.checkConnectivity());
+      // if (!check1) {
+      //   connect = false;
+      // } else {
+      //   connect = true;
+      // }
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        connect = true;
+      } else {
+        connect = false;
       }
+      return connect;
     } catch (e) {
-      throw Exception('Network connect error');
-      //  throw Exception('No connect network');
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        connect = true;
+      } else {
+        connect = false;
+      }
+      return connect;
     }
-    return false;
   }
 }
